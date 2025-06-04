cff987e431cea334a35ee9cf3441a9bd566320ef
Roman <romanprykhodkouae@mail.com>
2025-06-04 23:13:43 +0400

    task solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,20 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = ({ comment }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comment.name}</strong>
+
+      {' by '}
+
+      <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
+        {comment.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body">
+      {comment.body}
+      {/* dolorum voluptas laboriosam quisquam ab totam beatae et aut
+      aliquid optio assumenda voluptas velit itaque quidem voluptatem
+      tempore cupiditate in itaque sit molestiae minus dolores magni */}
+    </div>
+  </div>
+);
